1a5cbbb451f7ba541f65670e5e4892eb66852f6a
Sündüs Dinç <sundus.dinc01@hotmail.com>
2022-11-02 00:52:58 +0300

    Variable

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    // static variable salary
+    static double salary;
+    static String name = "Aks";
+
+    // Main Method
+    public static void Main(String[] args)
+    {
+
+        // accessing static variable
+        // without object
+        Program.salary = 100000;
+
+        Console.WriteLine(Program.name + "'s average salary:"
+                                           + Program.salary);
+    }
+
+
+
+
+}
